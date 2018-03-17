e649526e0ce579526775883a1fef26fcd1061996
Tracy Wallace <tswwci@hotmail.com>
2018-03-17 07:11:31 -0400

    Added configuration script

---
+++ PrepareDataDisk.ps1
@@ -0,0 +1,6 @@
+﻿$disk = Get-Disk  | ? {$_.PartitionStyle -eq "Raw"}
+$disk | Initialize-Disk 
+$partition = $disk | New-Partition -UseMaximumSize -AssignDriveLetter 
+$partition | Format-Volume -FileSystem NTFS
+git clone "https://github.com/MicrosoftLearning/20532-DevelopingMicrosoftAzureSolutions" F:\git
+Copy-Item -Path 'F:\git\Allfiles\dotnet\Mod*' -Destination 'F:\' -Recurse
