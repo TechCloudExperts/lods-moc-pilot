2a6707e4183f0dc5e04b11ac6e613815aff152d0
twallace27603 <tswwci@hotmail.com>
2018-03-30 09:56:22 -0400

    Tweaking the scripts

--- PrepareDataDisk.ps1
+++ PrepareDataDisk.ps1
@@ -3,9 +3,9 @@ $disk | Initialize-Disk
 $partition = $disk | New-Partition -UseMaximumSize -AssignDriveLetter 
 $partition | Format-Volume -FileSystem NTFS
 git clone "https://github.com/MicrosoftLearning/20532-DevelopingMicrosoftAzureSolutions" c:\LabSource
-Copy-Item -Path 'C:\git\Allfiles\dotnet\Mod*' -Destination 'F:\' -Recurse
-Copy-Item -Path '.\RefreshLab.ps1' -Destination 'c:\RefreshLab.ps1'
-$action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument '-File "c:\RefreshLab.ps1"'
-$trigger = New-ScheduledTaskTrigger -AtStartup
-$user = New-ScheduledTaskPrincipal -UserId "System"
-Register-ScheduledTask -Action $action -Trigger $trigger -User "System" -TaskName "UpdateLabFiles" -Description "Update all lab files"
+Copy-Item -Path 'C:\LabSource\Allfiles\dotnet\Mod*' -Destination 'F:\' -Recurse
+#Copy-Item -Path '.\RefreshLab.ps1' -Destination 'c:\RefreshLab.ps1'
+#$action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument '-File "c:\RefreshLab.ps1"'
+#$trigger = New-ScheduledTaskTrigger -AtStartup
+#$user = New-ScheduledTaskPrincipal -UserId "System"
+#Register-ScheduledTask -Action $action -Trigger $trigger -User "System" -TaskName "UpdateLabFiles" -Description "Update all lab files"
