b9d94bbe391b8bae155072987c5589e6b65f432e
twallace27603 <tswwci@hotmail.com>
2018-03-30 08:02:07 -0400

    Fixed reference to second script.

--- PrepareDataDisk.ps1
+++ PrepareDataDisk.ps1
@@ -4,8 +4,8 @@ $partition = $disk | New-Partition -UseMaximumSize -AssignDriveLetter
 $partition | Format-Volume -FileSystem NTFS
 git clone "https://github.com/MicrosoftLearning/20532-DevelopingMicrosoftAzureSolutions" c:\LabSource
 Copy-Item -Path 'C:\git\Allfiles\dotnet\Mod*' -Destination 'F:\' -Recurse
-Copy-Item -Path '.\RefreshLabs.ps1' -Destination 'c:\RefreshLabs.ps1'
-$action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument '-File "c:\RefreshLabs.ps1"'
+Copy-Item -Path '.\RefreshLab.ps1' -Destination 'c:\RefreshLab.ps1'
+$action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument '-File "c:\RefreshLab.ps1"'
 $trigger = New-ScheduledTaskTrigger -AtStartup
 $user = New-ScheduledTaskPrincipal -UserId "System"
 Register-ScheduledTask -Action $action -Trigger $trigger -User "System" -TaskName "UpdateLabFiles" -Description "Update all lab files"
